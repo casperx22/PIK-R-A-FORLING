ef43f473b6a4537d716fb0639732ac62f24d0eeb
casperx22 <alifbrianatmajaya121@gmail.com>
2026-02-01 14:09:32 +0700

    Refactor to open spreadsheet by ID and name

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function doPost(e) {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getSheets()[0]; 
+  var ss = SpreadsheetApp.openById("1ZSYjxlOxxrUVEcm8fVQ1NBgXH-TR8RGTey_b13bepyg");
+  var sheet = ss.getSheetByName("Sheet1");
   
   try {
     var data = JSON.parse(e.postData.contents);
@@ -14,13 +14,13 @@ function doPost(e) {
     ]);
     return ContentService.createTextOutput("Success").setMimeType(ContentService.MimeType.TEXT);
   } catch (err) {
-    return ContentService.createTextOutput("Error: " + err.message).setMimeType(ContentService.MimeType.TEXT);
+    return ContentService.createTextOutput("Error: " + err).setMimeType(ContentService.MimeType.TEXT);
   }
 }
 
 function doGet() {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getSheets()[0];
+  var ss = SpreadsheetApp.openById("1ZSYjxlOxxrUVEcm8fVQ1NBgXH-TR8RGTey_b13bepyg");
+  var sheet = ss.getSheetByName("Sheet1");
   var data = sheet.getDataRange().getValues();
   data.shift(); 
   return ContentService.createTextOutput(JSON.stringify(data)).setMimeType(ContentService.MimeType.JSON);
